eb4fdc76b2d2ecb1a8f4080ce9f1b36bba7dcaae
Madduri-Ganesh <mmaddur1@asu.edu>
2024-12-29 20:43:57 +0530

    backend sandbox deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import * as React from 'react';
+import {
+  createAmplifyAuthAdapter,
+  createStorageBrowser,
+} from '@aws-amplify/ui-react-storage/browser';
+import "@aws-amplify/ui-react-storage/styles.css";
+
 
 Amplify.configure(outputs);
+
+export const { StorageBrowser } = createStorageBrowser({
+  config: createAmplifyAuthAdapter(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
